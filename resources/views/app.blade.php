f0f46969325adfacd50822bf3baf96df73599c51
sho0322 <infinity0322s@gmail.com>
2022-09-04 13:31:39 +0900

    first_commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('/js/app.js') }}"></script>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
