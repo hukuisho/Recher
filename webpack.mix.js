1adf7de48e0e1a2fac789a97e6f660a576600f93
sho0322 <infinity0322s@gmail.com>
2022-09-12 23:42:57 +0900

    add:webpack--timeline.ts

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,8 @@ mix.ts("resources/ts/app.ts", "public/js");
 mix.ts("resources/ts/commons/header.ts", "public/js/commons/");
 mix.ts("resources/ts/commons/footer.ts", "public/js/commons/");
 mix.ts("resources/ts/guest/index.ts", "public/js/guest/");
+mix.ts(
+    "resources/ts/member/timeline/timeline.ts",
+    "public/js/member/timeline/"
+);
 mix.sass("resources/sass/app.scss", "public/css");
