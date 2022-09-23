c399ad21d876eb7b894bc561025a40d794ff9706
sho0322 <infinity0322s@gmail.com>
2022-09-23 19:25:05 +0900

    add

--- public/js/member/user/user.js
+++ public/js/member/user/user.js
@@ -1,12 +1,14 @@
-/******/ (() => {
-    // webpackBootstrap
-    /******/ "use strict";
-    /******/
-    /******/
-    var __webpack_exports__ = {};
-    /*!******************************************!*\
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!******************************************!*\
   !*** ./resources/ts/member/user/user.ts ***!
   \******************************************/
-
-    /******/
-})();
+ // addEventListener("DOMContentLoaded", () => {
+//     const userEditButton = document.querySelector("#userEditButton");
+//     userEditButton?.addEventListener("click", () => {
+//         location.href = "/member/user";
+//     });
+// });
+/******/ })()
+;
