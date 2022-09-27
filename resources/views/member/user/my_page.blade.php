a823405479bd377df4af338d15b44ee8e10c0e30
sho0322 <infinity0322s@gmail.com>
2022-09-27 23:37:36 +0900

    link

--- resources/views/member/user/my_page.blade.php
+++ resources/views/member/user/my_page.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.layouts')
 @section('content')
     <div class="my_page">
-        <img id="userIcon" class="my_page__userIcon" src="{{ Auth::user()->profile_image }}" alt="アイコン">
+        <a href="/member/user/edit/{{ Auth::user()->id }}">
+            <img id="userIcon" class="my_page__userIcon" src="{{ Auth::user()->profile_image }}" alt="アイコン">
+        </a>
         <h1 class="my_page__userName">{{ Auth::user()->name }}</h1>
         <a class="my_page__relationship-link" href="">関係</a>
         <a class="my_page__edit-link" href="/member/user/edit/{{ Auth::user()->id }}">編集</a>
