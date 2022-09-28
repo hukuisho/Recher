3001d8a1e3ac38373501db0cb58dfbd189f66ca3
sho0322 <infinity0322s@gmail.com>
2022-09-28 21:30:55 +0900

    バリデーション

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -21,12 +21,12 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input)
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:30'],
             'email' => [
                 'required',
                 'string',
                 'email',
-                'max:255',
+                'max:30',
                 Rule::unique(User::class),
             ],
             'password' => $this->passwordRules(),
