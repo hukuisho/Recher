6188126090faacb85e871c9f163fcc30f82a4917
sho0322 <infinity0322s@gmail.com>
2022-09-28 21:20:57 +0900

    fix

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,35 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('/js/app.js') }}"></script>
+    @switch ($_SERVER['REQUEST_URI'])
+    @case("/")
+        <title>非会員TOP</title>
+        @break
+    @case ("/member")
+        <title>会員TOP</title>
+        @break
+    @case ("/member/timeline")
+        <title>投稿</title>
+        @break
+    @case ("/member/schedule")
+        <title>予定</title>
+        @break
+    @case ("/member/search")
+        <title>検索</title>
+        @break
+    @case ("/member/bell")
+        <title>通知</title>
+        @break
+    @case ("/member/renraku")
+        <title>連絡</title>
+        @break
+    @default
+        @if(strpos($_SERVER['REQUEST_URI'],'user') == true)
+            <title>マイページ</title>
+            @else 
+            <title>Recher - 理想の生き方を見つけよう -</title>
+        @endif
+    @endswitch
+</head>
--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -1,27 +1,5 @@
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('/js/app.js') }}"></script>
-        @switch ($_SERVER['REQUEST_URI'])
-        @case("/")
-            <title>非会員TOP</title>
-            @break
-        @case ("/member")
-            <title>会員TOP</title>
-            @break
-        @case ("/member/search")
-            <title>検索</title>
-            @break
-        @default
-            @if(strpos($_SERVER['REQUEST_URI'],'user') == true)
-                <title>マイページ</title>
-                @else 
-                <title>Recher - 理想の生き方を見つけよう -</title>
-            @endif
-        @endswitch
-    </head>
+@include('layouts.head')
     <body>
         <div id="richer" class="richer">
             @include('layouts.header')
@@ -32,28 +10,6 @@
             </div>
             @include('layouts.footer')
         </div>
-        @unless ($_SERVER['REQUEST_URI'] == "/")
-            <script src="{{ asset('/js/commons/header.js') }}"></script>
-            <script src="{{ asset('/js/commons/footer.js') }}"></script>
-        @endunless
-        @switch ($_SERVER['REQUEST_URI'])
-            @case("/")
-                <script src="{{ asset('/js/guest/index.js') }}"></script>
-            @break
-            @case("/member")
-                <script src="{{ asset('/js/member/top/top.js') }}"></script>
-            @break
-            @case ("/member/search")
-                <script src="{{ asset('/js/member/search/search.js') }}"></script>
-            @break
-            @default
-            @if(strpos($_SERVER['REQUEST_URI'],'user') == true)
-                <script src="{{ asset('/js/member/user/user.js') }}"></script>
-                @break
-                @else 
-                    <script src="{{ asset('/js/member/user/user.js') }}"></script>
-                @break
-            @endif
-        @endswitch
+        @include('layouts.js')
     </body>
 </html>
