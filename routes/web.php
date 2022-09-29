6195629cfded053217be8413a41e08bad1551aa2
sho0322 <infinity0322s@gmail.com>
2022-09-30 00:42:29 +0900

    fix

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,17 @@ Route::fallback(function(){
 });
 Route::get('/notPage', [CommonController::class, 'notPage']);
 //非会員
-Route::get('/', [GuestController::class, 'top']);
+Route::get('/',[GuestController::class, 'top']);
 //会員
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/member', [MemberController::class, 'top']);
-    Route::get('/member/user', [MemberController::class, 'my_page']);
-    Route::get('/member/user/show/{id}', [MemberController::class, 'profile_show']);
-    Route::get('/member/user/edit/{id}', [MemberController::class, 'profile_edit']);
+    Route::get('/member',                 [MemberController::class, 'top']);
+    Route::get('/member/user',            [MemberController::class, 'my_page']);
+    Route::get('/member/user/show/{id}',  [MemberController::class, 'profile_show']);
+    Route::get('/member/user/edit/{id}',  [MemberController::class, 'profile_edit']);
     Route::post('/member/user/edit/{id}', [MemberController::class, 'profile_edit']);
-    Route::get('/member/timeline', [MemberController::class, 'timeline_index']);
-    Route::get('/member/schedule', [MemberController::class, 'schedule_index']);
-    Route::get('/member/bell', [MemberController::class, 'bell_index']);
-    Route::get('/member/renraku', [MemberController::class, 'renraku_index']);
-    Route::get('/member/search', [MemberController::class, 'search']);
+    Route::get('/member/timeline',        [MemberController::class, 'timeline_index']);
+    Route::get('/member/schedule',        [MemberController::class, 'schedule_index']);
+    Route::get('/member/bell',            [MemberController::class, 'bell_index']);
+    Route::get('/member/renraku',         [MemberController::class, 'renraku_index']);
+    Route::get('/member/search',          [MemberController::class, 'search']);
 });
