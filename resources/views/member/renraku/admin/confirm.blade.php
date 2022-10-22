2596a2842383c9e745e321f64280a46307c30c87
sho0322 <infinity0322s@gmail.com>
2022-10-22 16:48:39 +0900

    confirm:fix-text-color

--- resources/views/member/renraku/admin/confirm.blade.php
+++ resources/views/member/renraku/admin/confirm.blade.php
@@ -4,13 +4,13 @@
   @csrf
     <div class="renraku__admin-confirm">
         <label class="renraku__admin-confirm__menu">メールアドレス</label>
-        {{ $inputs['email'] }}
+        <h3 class="renraku__admin-confirm__menu__value">{{ $inputs['email'] }}</h3>
         <input name="email" value="{{ $inputs['email'] }}" type="hidden">
         <label class="renraku__admin-confirm__menu">タイトル</label>
-        {{ $inputs['title'] }}
+        <h3 class="renraku__admin-confirm__menu__value">{{ $inputs['title'] }}</h3>
         <input name="title" value="{{ $inputs['title'] }}" type="hidden">
         <label class="renraku__admin-confirm__menu">お問い合わせ内容</label>
-        {!! nl2br(e($inputs['body'])) !!}
+        <h3 class="renraku__admin-confirm__menu__value">{!! nl2br(e($inputs['body'])) !!}<h5>
         <input name="body" value="{{ $inputs['body'] }}" type="hidden">
         <button type="submit" name="action" value="submit">送信する</button>
         <button><a href="/member/renraku/admin">入力内容修正</a></button>
