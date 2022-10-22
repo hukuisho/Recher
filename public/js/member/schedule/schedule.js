3f663f0104da122435a8babbe4f1fd4498842ab2
sho0322 <infinity0322s@gmail.com>
2022-10-22 16:12:52 +0900

    フィックス

--- public/js/member/schedule/schedule.js
+++ public/js/member/schedule/schedule.js
@@ -1,13 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-var __webpack_exports__ = {};
-/*!***********************************************************!*\
-  !*** ./resources/ts/react/components/member/Schedule.tsx ***!
-  \***********************************************************/
-
-
-addEventListener("DOMContentLoaded", function () {
-  alert("dfghj");
-});
+/******/ 	
+/******/ 	
 /******/ })()
 ;
